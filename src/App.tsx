6313918914bc5c134b89fee89f9fe01630bbbfa7
Victor Li <victorli5674@yahoo.com>
2022-12-14 17:51:43 -0800

    hook up create account api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { createMachine, assign } from "xstate";
 import { useMachine } from "@xstate/react";
 import { inspect } from "@xstate/inspect";
-import { ActionGroup, Item } from "@adobe/react-spectrum";
+import { ActionGroup, Item, TextField } from "@adobe/react-spectrum";
 import "./App.css";
 import React from "react";
 
@@ -189,7 +189,20 @@ const questionMachine = createMachine(
         on: { next: "email", prev: "phone" },
       },
       email: {
-        on: { next: "ssn", prev: "name" },
+        on: {
+          next: [{ target: "accountCreate", actions: ["saveEmail"] }],
+          prev: "name",
+        },
+      },
+      accountCreate: {
+        invoke: {
+          id: "accountCreate",
+          src: "createAccount",
+          onDone: {
+            target: "ssn",
+            actions: "accountCreateOnDone",
+          },
+        },
       },
       ssn: {
         type: "final",
@@ -230,6 +243,91 @@ const questionMachine = createMachine(
           howItWorks: true,
         };
       }),
+      saveEmail: assign((context, event) => {
+        return {
+          email: event.email,
+        };
+      }),
+      accountCreateOnDone: assign((context, event) => {
+        console.log("account on done");
+
+        console.log({ event });
+
+        return {
+          user: event.data.user,
+          identity: event.data.identity,
+          policy: event.data.policy,
+        };
+      }),
+    },
+    services: {
+      createAccount: (context, event) => {
+        const body = {
+          userIdentityEmailData: {
+            nameFirst: "TRLTestCase",
+            nameMiddle: "Z",
+            nameLast: "One",
+            birthDate: "1990-09-09",
+            gender: "Female",
+            email: event.email,
+            recaptcha: null,
+            birthState: "AK",
+            birthCountry: "USA",
+            partnerCode: null,
+          },
+          addressData: {
+            street1: "4624 South Pacific Highway",
+            region: "OR",
+            locality: "Phoenix",
+            postalCode: "97535",
+            type: "primary",
+          },
+          phoneNumberData: { number: "(823) 940-8239", type: "primary" },
+          root: "http://localhost:9002",
+          createAccountOnly: true,
+          agentAssistedApplication: false,
+        };
+
+        return fetch("http://localhost:8000/v2.0/create-account", {
+          headers: {
+            accept: "application/json",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(body),
+          method: "PUT",
+          credentials: "include",
+        })
+          .then(async (res) => {
+            const json = await res.json();
+
+            return { xcsrftoken: res.headers.get("X-CSRF-TOKEN"), json };
+          })
+          .then(async ({ json, xcsrftoken }) => {
+            const policyJson = await fetch(
+              `http://localhost:8000/v2.0/user/${json.user.id}/identity/${json.identity.id}/term-policy`,
+              {
+                headers: {
+                  accept: "application/json",
+                  "content-type": "application/json",
+                  "X-XSRF-TOKEN": xcsrftoken,
+                },
+                body: '{"optionalData":{"profileData":{"statusCitizen":true,"statusPermanentResident":false,"prescreen":{"declaredTobacco":false,"declaredHealth":"3"},"occupationalHistory":{"grossAnnualIncome":"0"}},"quoteData":{}},"paymentPlan":"monthly","clientData":{"activeInUi":true,"attributionData":{},"intent":"veryLow","intentAnswer":"I\'m not sure","flow":"nap-recommendation","partnerCode":"","createdByPartner":false,"telesales":0,"progress":"preinterview","partnershipAttributes":{},"children":"0","mortgageDebtAmounts":"0","iulCandidate":false},"carrier":"LGA","purposeOfInsurance":"personal","applicationSource":null}',
+                method: "PUT",
+                credentials: "include",
+              }
+            );
+
+            const policy = await policyJson.json();
+
+            return new Promise((res) => {
+              res({
+                user: json.user,
+                identity: json.identity,
+                policy,
+              });
+            });
+          });
+      },
     },
   }
 );
@@ -240,6 +338,7 @@ function Question({ name, send, context }) {
   );
   let [goalsSelected, goalsSetSelected] = React.useState(new Set([]));
   let [birthCountry, setBirthCountry] = React.useState();
+  let [email, setEmail] = React.useState("");
 
   if (name === "protections") {
     return (
@@ -306,9 +405,33 @@ function Question({ name, send, context }) {
         </button>
       </>
     );
+  } else if (name === "email" || name === "accountCreate") {
+    return (
+      <>
+        <h1>Question: {name}</h1>
+        <div>
+          <TextField
+            label="email"
+            value={email}
+            onChange={setEmail}
+            isDisabled={name === "accountCreate"}
+          />
+        </div>
+        <button
+          onClick={() => send("prev")}
+          disabled={name === "accountCreate"}
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => send({ type: "next", email })}
+          disabled={name === "accountCreate"}
+        >
+          Next
+        </button>
+      </>
+    );
   }
-
-  // skip questions
 
   return (
     <>
@@ -329,7 +452,12 @@ export default function App() {
   }
 
   if (state.done) {
-    return <h1>Final Question: {state.value}</h1>;
+    return (
+      <>
+        <h1>Final Question: {state.value}</h1>
+        <h2>{JSON.stringify({ policy: state.context.policy }, null, 2)}</h2>
+      </>
+    );
   }
 
   return (
